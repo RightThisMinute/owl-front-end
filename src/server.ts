89e954e0c846e953102842e938f79214f2e45ddc
Donovan Mueller <zotobi@gmail.com>
2017-07-06 17:07:47 -0700

    Add watching

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,59 @@
 
-setTimeout(() => {
-	console.log('hi there')
-}, 300)
+import * as express from 'express'
+import * as http from 'http'
+import * as logger from 'morgan'
+
+import * as createDebug from 'debug'
+const debug = createDebug('server')
+
+
+const server = express()
+server.use(logger('dev'))
+
+const router = express.Router()
+// placeholder route handler
+router.get('/', (req, res, next) => {
+	res.json({
+		message: 'Hello, World!'
+	})
+})
+server.use('/', router)
+
+const port = normalizePort(process.env.PORT || 3001)
+server.set('port', port)
+
+const httpServer = http.createServer(server)
+httpServer.listen(port)
+httpServer.on('error', onError)
+httpServer.on('listening', onListening)
+
+
+function normalizePort(val: number|string): number|string|boolean {
+	let port: number = (typeof val === 'string') ? parseInt(val, 10) : val
+	if (isNaN(port))return val
+	else if (port >= 0) return port
+	else return false
+}
+
+function onError(error: NodeJS.ErrnoException): void {
+	if (error.syscall !== 'listen') throw error
+	let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port
+	switch(error.code) {
+		case 'EACCES':
+			console.error(`${bind} requires elevated privileges`)
+			process.exit(1)
+			break
+		case 'EADDRINUSE':
+			console.error(`${bind} is already in use`)
+			process.exit(1)
+			break
+		default:
+			throw error
+	}
+}
+
+function onListening(): void {
+	let addr = httpServer.address()
+	let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`
+	debug(`Listening on ${bind}`)
+}
