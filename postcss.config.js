39eb13dbb9d520243453c8db85421afbb97ae2bc
Donovan Mueller <zotobi@gmail.com>
2017-08-25 15:44:22 -0700

    Prep CSS for masonry.

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
+
 module.exports = {
 	parser: require('postcss-scss'), // For inline comment support in .pcss files.
 	plugins: [
 		require('postcss-import'),
-		require('postcss-nested'),
 		require('postcss-property-lookup'),
 		require('postcss-css-variables'),
+		require('postcss-nested'),
 		require('postcss-color-function'),
 		require('autoprefixer'),
 	]
